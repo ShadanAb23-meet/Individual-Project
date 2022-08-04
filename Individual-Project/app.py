3a1fc072539eeb1797407c1c627432b024339582
ShadanAb23-meet <109904715+ShadanAb23-meet@users.noreply.github.com>
2022-08-04 12:45:40 +0300

    done

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -39,16 +39,32 @@ def signup():
 	if request.method == 'POST':
 		email = request.form['email']
 		password = request.form['password']
+		bio = request.form['bio']
+		username = request.form['username']
+		full_name = request.form['full_name']
 		try:
 			login_session['user'] = auth.create_user_with_email_and_password(email, password)
-			return redirect(url_for('index.html'))
+			user = {'bio': bio, 'email': email, 'password': password, 'username': username, 'full_name': full_name}
+			db.child("user").child(login_session['user']['localId']).set(user)			
+
+			return redirect(url_for('homepage'))
+			
 		except:
 			error = "Authentication failed"
+		
 	return render_template("signup.html")
 
-@app.route('/')
+@app.route('/home')
 def homepage():
-	return render_template("home.html")
+	user = db.child("user").child(login_session['user']['localId']).get().val()
+	full_name = user["full_name"]
+
+	return render_template("home.html", full_name=full_name)
+
+
+@app.route('/')
+def home2():
+	return render_template("index.html")
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
@@ -56,23 +72,30 @@ def signin():
 	if request.method == 'POST':
 		email = request.form['email']
 		password = request.form['password']
+		login_session["user"]= auth.sign_in_with_email_and_password(email, password)
 		try:
-			login_session["user"]= auth.sign_in_with_email_and_password(email, password)
-			return redirect(url_for('signin'))
+			return redirect(url_for('homepage'))
 		except:
 			error = "Authentication failed"
 			return render_template("signin.html")
 	return render_template("signin.html")
 
-@app.route('/about_us')
-def about_us():
-	return render_template("about_us.html")
+@app.route('/about')
+def about():
+	return render_template("about.html")
 
 @app.route('/css')
 def css():
 	return render_template("Css.html")
 
+@app.route('/idea')
+def idea():
+	return render_template("idea.html")
 
+
+@app.route('/why')
+def why():
+	return render_template("why.html")
 
 
 #Code goes above here
